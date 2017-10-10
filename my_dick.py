f6402559a62ec42a2a6a0b6b3039135f41b45a3c
meganrumph <mmrumph@loyola.edu>
2017-10-10 19:08:05 -0400

    cocks

---
+++ my_dick.py
@@ -0,0 +1,9 @@
+my_dick = float(input("How big is your dick in racks?\n"))
+if my_dick >= 3:
+    print ("That is a massive cock\n")
+    convert= input("Would you like to convert that into inches?\n\n... Of course you would you dirty bastard\n")
+    if convert == "yes":
+        inches = my_dick*1.75
+        print("Your dick is",inches,"inches\n")
+
+
